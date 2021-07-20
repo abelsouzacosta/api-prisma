0b07b22d50e3e0638b950a4b5d557d1849839297
Abel Souza <abelsouzacosta@gmail.com>
2021-07-20 11:30:40 -0300

    feat(errors): adicionando middleware de tratamento de erros da aplicação
    Permite lançar erros de qualquer lugar da aplicação de forma com que haja um tratamento de erros
    homogêneo, sendo todos os erros instâncias de uma mesma classe

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,28 @@
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
+import ApplicationError from '@errors/ApplicationError';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// middleware de errors
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApplicationError)
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+
+    return response.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
+  },
+);
 
 app.listen(process.env.PORT);
